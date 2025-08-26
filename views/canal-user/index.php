e5eed1e98e15e1297a877b0244e9a4ef775e92db
miriamperalta <peraltamiriam26@gmail.com>
2025-08-26 07:47:44 -0300

    correccion check es obligatorio

--- views/canal-user/index.php
+++ views/canal-user/index.php
@@ -7,6 +7,7 @@ use kartik\dialog\Dialog;
 use webzop\notifications\model\CanalUser;
 use webzop\notifications\model\TipoNotificacionCanal;
 use backend\assets\AppAsset;
+use yii\helpers\VarDumper;
 
 AppAsset::register($this);
 
@@ -39,7 +40,8 @@ echo Dialog::widget(['overrideYiiConfirm' => true]);
                     /** buscar la asociación de canal y notificación */
                     $notificacionCanal = CanalUser::buscarPorUsuario($modelCanalEmail->id, $model->id, $user_id);
                     $tipoNotCanal = TipoNotificacionCanal::buscar($modelCanalEmail->id, $model->id);
-                    if (isset($tipoNotCanal->es_seleccionable)) {
+                   
+                    if (isset($tipoNotCanal->es_seleccionable) && ($tipoNotCanal->es_seleccionable == 1)) {
                         $info =  Html::tag('span',
                                             '<i class="fas fa-info-circle"></i>',
                                             [
@@ -49,9 +51,7 @@ echo Dialog::widget(['overrideYiiConfirm' => true]);
                                                 'title' => 'La notificación es obligatoria.',
                                                 'data-bs-html' => 'true'
                                             ]
-                                        );
-
-                      
+                                        );                      
                         $disabled = true;
                     }else{
                         $disabled = false;
@@ -76,15 +76,32 @@ echo Dialog::widget(['overrideYiiConfirm' => true]);
                     /** buscar la asociación de canal y notificación */
                     $notificacionCanal = CanalUser::buscarPorUsuario($modelCanalSystem->id, $model->id, $user_id);
                     $tipoNotCanal = TipoNotificacionCanal::buscar($modelCanalSystem->id, $model->id);
+
+                    if (isset($tipoNotCanal->es_seleccionable) && ($tipoNotCanal->es_seleccionable == 1)) {
+                        $info =  Html::tag('span',
+                                            '<i class="fas fa-info-circle"></i>',
+                                            [
+                                                'class' => 'ps-2',
+                                                'style' => 'color:#3584e4',
+                                                'data-bs-toggle' => 'tooltip',
+                                                'title' => 'La notificación es obligatoria.',
+                                                'data-bs-html' => 'true'
+                                            ]
+                                        );                      
+                        $disabled = true;
+                    }else{
+                        $disabled = false;
+                        $info = "";
+                    }
                     return html::checkbox('check_notify',false,[
                             'value' => $model->id,
                             'checked' => !empty($notificacionCanal) ? true : false,
                             'onclick'=> 'checkAsociarAusentismo(this, '.$modelCanalSystem->id.', '.$model->id.', "notifications/canal-user/add")',
                             'class' => 'check',
-                            'disabled' => isset($tipoNotCanal->es_seleccionable) ? true : false
+                            'disabled' => $disabled
 
                         ]
-                    ); 
+                    ).$info; 
                 },
                 'header' => 'Notificar por sistema',
                 'rowHighlight' => false
